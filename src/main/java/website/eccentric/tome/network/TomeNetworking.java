dc3f5a6602065f1e25792de135e52e7d4d607ca5
thepigcat <benpospo@gmail.com>
2024-10-10 19:32:10 +0200

    fix server issue

--- src/main/java/website/eccentric/tome/network/TomeNetworking.java
+++ src/main/java/website/eccentric/tome/network/TomeNetworking.java
@@ -8,15 +8,12 @@ import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import website.eccentric.tome.EccentricTome;
 
-@EventBusSubscriber(modid = EccentricTome.ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = EccentricTome.ID, bus = EventBusSubscriber.Bus.MOD)
 public class TomeNetworking {
-    public static final ResourceLocation name = ResourceLocation.fromNamespaceAndPath(EccentricTome.ID, "general");
-    public static final String version = ResourceLocation.fromNamespaceAndPath(EccentricTome.ID, "1").toString();
-
     @SubscribeEvent
     public static void registerPayloads(RegisterPayloadHandlersEvent event) {
         PayloadRegistrar registrar = event.registrar(EccentricTome.ID);
-        registrar.playBidirectional(RevertPayload.TYPE, RevertPayload.STREAM_CODEC, RevertPayload::revert);
-        registrar.playBidirectional(ConvertPayload.TYPE, ConvertPayload.STREAM_CODEC, ConvertPayload::convert);
+        registrar.playToServer(RevertPayload.TYPE, RevertPayload.STREAM_CODEC, RevertPayload::revert);
+        registrar.playToServer(ConvertPayload.TYPE, ConvertPayload.STREAM_CODEC, ConvertPayload::convert);
     }
 }
